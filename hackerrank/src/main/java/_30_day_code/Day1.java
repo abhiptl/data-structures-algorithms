d3561b884c142e8a9e4db2f3d6da376a7ed5298e
Abhishek Patel <abhishek.patel@sanelib.com>
2017-11-02 16:22:48 +0530

    Day 0: Hello, World

---
+++ hackerrank/src/main/java/_30_day_code/Day1.java
@@ -0,0 +1,16 @@
+package _30_day_code;
+
+import java.util.Scanner;
+
+/**
+ * @link https://www.hackerrank.com/challenges/30-hello-world/problem
+ */
+public class Day1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.nextLine();
+        scanner.close();
+        System.out.println("Hello, World.");
+        System.out.println(inputString);
+    }
+}
