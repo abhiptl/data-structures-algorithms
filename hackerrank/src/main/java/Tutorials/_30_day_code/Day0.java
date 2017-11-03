ee4d8c52498e32be05b12e251230d28265209e74
Abhishek Patel <abhishek.patel@sanelib.com>
2017-11-03 10:50:13 +0530

    Repackaging

--- hackerrank/src/main/java/Tutorials/_30_day_code/Day0.java
+++ hackerrank/src/main/java/Tutorials/_30_day_code/Day0.java
@@ -1,11 +1,11 @@
-package _30_day_code;
+package Tutorials._30_day_code;
 
 import java.util.Scanner;
 
 /**
  * @link https://www.hackerrank.com/challenges/30-hello-world/problem
  */
-public class Day1 {
+public class Day0 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String inputString = scanner.nextLine();
