c8fddb416a5a368d5a270151bd8ea47b16f812e6
Abhishek Patel <abhishek.patel@sanelib.com>
2017-11-07 16:30:59 +0530

    Day2Operators - use of division with double

---
+++ hackerrank/src/main/java/Tutorials/_30_day_code/Day2Operators.java
@@ -0,0 +1,24 @@
+package Tutorials._30_day_code;
+
+import java.util.Scanner;
+
+public class Day2Operators {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        double mealCost = scan.nextDouble(); // original meal price
+        int tipPercent = scan.nextInt(); // tip percentage
+        int taxPercent = scan.nextInt(); // tax percentage
+        scan.close();
+
+        // Write your calculation code here.
+
+        double tip = mealCost * (tipPercent / 100d);
+        double tax = mealCost * (taxPercent / 100d);
+
+        double total = mealCost + tip + tax;
+        // cast the result of the rounding operation to an int and save it as totalCost
+        int totalCost = (int) Math.round(total);
+        System.out.printf("The total meal cost is %d dollars.", totalCost);
+        // Print your result
+    }
+}
