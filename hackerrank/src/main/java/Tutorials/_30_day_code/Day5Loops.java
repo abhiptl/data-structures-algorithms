23a824a4903ebf24f7bb19183752f1ac92331b21
Abhishek Patel <abhishek.patel@sanelib.com>
2017-11-10 09:24:29 +0530

    Day5 loops

---
+++ hackerrank/src/main/java/Tutorials/_30_day_code/Day5Loops.java
@@ -0,0 +1,18 @@
+package Tutorials._30_day_code;
+
+import java.util.Scanner;
+
+public class Day5Loops {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        int result;
+        for(int i = 1; i<= 10; i++) {
+            result = n * i;
+
+            System.out.printf("%d x %d = %d", n, i, result);
+            System.out.println("");
+        }
+    }
+}
