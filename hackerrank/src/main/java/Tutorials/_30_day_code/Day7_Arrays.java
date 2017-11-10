9490d2f5638cb49f0a4ab62dd86365ee84f63d6e
Abhishek Patel <abhishek.patel@sanelib.com>
2017-11-10 09:49:19 +0530

    Day7 - Arrays

--- hackerrank/src/main/java/Tutorials/_30_day_code/Day7_Arrays.java
+++ hackerrank/src/main/java/Tutorials/_30_day_code/Day7_Arrays.java
@@ -1,4 +1,20 @@
 package Tutorials._30_day_code;
 
+import java.util.Scanner;
+
 public class Day7_Arrays {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for(int i=n-1; i >=0; i--){
+            arr[i] = in.nextInt();
+        }
+        in.close();
+
+        for(int i = 0;i < n; i++) {
+            System.out.print(arr[i]);
+            System.out.print(" ");
+        }
+    }
 }
