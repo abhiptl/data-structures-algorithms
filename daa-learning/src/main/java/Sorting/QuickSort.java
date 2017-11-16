75bfd09ea4669732e0400328b229a47e3600f072
Abhishek Patel <abhishek.patel@sanelib.com>
2017-11-16 22:35:54 +0530

    Quick sort

---
+++ daa-learning/src/main/java/Sorting/QuickSort.java
@@ -0,0 +1,61 @@
+package Sorting;
+
+import java.util.Scanner;
+
+/**
+ * Worst case : o(n2)
+ * Best case : nLogn
+ * Average case : nLogn
+ */
+public class QuickSort {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        doQuickSort(arr, 0, args.length - 1);
+
+        for(int i = 0; i< arr.length;i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    private static void doQuickSort(int[] arr, int low, int high) {
+        if(low < high) {
+            int pivot = partition(arr, low, high);
+            doQuickSort(arr, low, pivot - 1);
+            doQuickSort(arr, pivot + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = (low-1); // index of smaller element
+        for (int j=low; j<high; j++)
+        {
+            // If current element is smaller than or
+            // equal to pivot
+            if (arr[j] <= pivot)
+            {
+                i++;
+
+                // swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+
+        // swap arr[i+1] and arr[high] (or pivot)
+        int temp = arr[i+1];
+        arr[i+1] = arr[high];
+        arr[high] = temp;
+
+        return i+1;
+    }
+
+}
