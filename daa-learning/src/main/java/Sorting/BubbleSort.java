0e652a095c985f11470df3454c2fecddff576828
Abhishek Patel <abhishek.patel@sanelib.com>
2017-11-16 15:07:17 +0530

    Optimized bubble sort

--- daa-learning/src/main/java/Sorting/BubbleSort.java
+++ daa-learning/src/main/java/Sorting/BubbleSort.java
@@ -12,6 +12,16 @@ public class BubbleSort {
             unsorted[i] = in.nextInt();
         }
 
+        //bubbleSort(unsorted, n);
+        optimizedBubbleSort(unsorted, n);
+
+        for(int i = 0; i < n; i++){
+            System.out.println(unsorted[i]);
+        }
+
+    }
+
+    public static void bubbleSort(int[] unsorted, int n) {
         for(int i = 0; i < n - 1; i++) {
             for(int j = 0; j < n - i - 1; j++) {
                 if(unsorted[j] > unsorted[j+1]) {
@@ -20,11 +30,25 @@ public class BubbleSort {
             }
         }
 
-        for(int i = 0; i < n; i++){
-            System.out.println(unsorted[i]);
+    }
+
+    public static void optimizedBubbleSort(int[] unsorted, int n) {
+        for(int i = 0; i < n - 1; i++) {
+            boolean swapHappen = false;
+            for(int j = 0; j < n - i - 1; j++) {
+                if(unsorted[j] > unsorted[j+1]) {
+                    swapNumbers(unsorted, j ,j+1);
+                    swapHappen = true;
+                }
+            }
+
+            if(!swapHappen) {
+                break;
+            }
         }
 
     }
+
 
     public static void swapNumbers(int[] arr, int m, int n) {
         int temp = arr[m];
