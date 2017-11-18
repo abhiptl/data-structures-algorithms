88082862f0ea60f3ace3d63e11ddb5adbb839a5b
Abhishek Patel <abhishek.patel@sanelib.com>
2017-11-18 17:37:18 +0530

    Second solution to get max from stack

--- hackerrank/src/main/java/DataStructures/Stack/easy/MaximumElement.java
+++ hackerrank/src/main/java/DataStructures/Stack/easy/MaximumElement.java
@@ -1,7 +1,5 @@
 package DataStructures.Stack.easy;
 
-import javafx.collections.transformation.SortedList;
-
 import java.math.BigInteger;
 import java.util.*;
 
@@ -23,27 +21,16 @@ public class MaximumElement {
             if(type == 1) {
                 newElement = scanner.nextBigInteger();
                 stack.push(newElement);
+
             }
 
             if(type == 2) {
-                stack.pop();
+                BigInteger pop = stack.pop();
             }
 
             if(type == 3) {
-                Enumeration<BigInteger> elements = stack.elements();
-                BigInteger maxElement = null;
-                while (elements.hasMoreElements()) {
-                    if(maxElement == null) {
-                        maxElement = elements.nextElement();
-                    } else {
-                        BigInteger bigInteger = elements.nextElement();
-                        if(bigInteger.compareTo(maxElement) > 0) {
-                            maxElement = bigInteger;
-                        }
-                    }
-                }
-                list.add(maxElement);
-
+                Optional<BigInteger> max = stack.stream().max(BigInteger::compareTo);
+                list.add(max.get());
             }
         }
 
