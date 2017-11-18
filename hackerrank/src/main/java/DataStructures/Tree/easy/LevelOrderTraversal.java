52babab78d6339591f0957acac204690807e823d
Abhishek Patel <abhishek.patel@sanelib.com>
2017-11-18 12:20:03 +0530

    Level order traversal easy way

--- hackerrank/src/main/java/DataStructures/Tree/easy/LevelOrderTraversal.java
+++ hackerrank/src/main/java/DataStructures/Tree/easy/LevelOrderTraversal.java
@@ -24,5 +24,52 @@ public class LevelOrderTraversal {
 
     }
 
+    /* Function to line by line print level order traversal a tree*/
+    void printLevelOrder(Node root)
+    {
+        int h = height(root);
+        int i;
+        for (i=1; i<=h; i++)
+        {
+            printGivenLevel(root, i);
+            System.out.println();
+        }
+    }
+
+    /* Compute the "height" of a tree -- the number of
+    nodes along the longest path from the root node
+    down to the farthest leaf node.*/
+    int height(Node root)
+    {
+        if (root == null)
+            return 0;
+        else
+        {
+            /* compute  height of each subtree */
+            int lheight = height(root.left);
+            int rheight = height(root.right);
+
+            /* use the larger one */
+            if (lheight > rheight)
+                return(lheight+1);
+            else return(rheight+1);
+        }
+    }
+
+    /* Print nodes at a given level */
+    void printGivenLevel(Node root, int level)
+    {
+        if (root == null)
+            return;
+        if (level == 1)
+            System.out.println(root.data);
+        else if (level > 1)
+        {
+            printGivenLevel(root.left, level-1);
+            printGivenLevel(root.right, level-1);
+        }
+    }
 
 }
+
+
