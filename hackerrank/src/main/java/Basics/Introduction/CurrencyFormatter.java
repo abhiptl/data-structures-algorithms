e35fc2af5175d16a811a1abdd8c1e9666d31efa3
Abhishek Patel <abhi3290@gmail.com>
2020-03-26 15:46:40 +0530

    Currency formatter

---
+++ hackerrank/src/main/java/Basics/Introduction/CurrencyFormatter.java
@@ -0,0 +1,31 @@
+package Basics.Introduction;
+
+import java.text.NumberFormat;
+import java.util.Currency;
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/java-currency-formatter/problem
+ */
+public class CurrencyFormatter {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
+
+        NumberFormat lp;  //Local Payment
+
+        lp = NumberFormat.getCurrencyInstance(Locale.US);
+        System.out.println("US: " + lp.format(payment));
+
+        lp = NumberFormat.getCurrencyInstance(new Locale("en", "in"));
+        System.out.println("India: " + lp.format(payment));
+
+        lp = NumberFormat.getCurrencyInstance(Locale.CHINA);
+        System.out.println("China: " + lp.format(payment));
+
+        lp = NumberFormat.getCurrencyInstance(Locale.FRANCE);
+        System.out.println("France: " + lp.format(payment));
+    }
+}
